153919e0dac186648bfc1e8aa31fc87db81b978b
luizokada <luizfernandookada@gmail.com>
2023-03-29 12:51:33 -0300

    :edit: simpleSum

--- tests/simpleSum.c
+++ tests/simpleSum.c
@@ -1,9 +1,9 @@
 int main(int a,char b,float c){
-    1 + 5 + 6;
+    
     5 + 3 + 5;
     10 + 10;
-    int a = 20;
+    int a = 1 + 5 + 6;
     a = 10;
-    int c = 10;
-    int 6;
+    int b;
+    int c = a + b;
 } /*AAAAAAAAAAAAAAAAAAAAAAAAAAAA*/
