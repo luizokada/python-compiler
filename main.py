a9922c6f56b73d55eec4b147a4fb37b6fe2f4cb0
luizokada <luizfernandookada@gmail.com>
2023-04-07 17:35:09 -0300

    feat: pass archieve name per apram

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 from components.lexer import Lexer
 from components.parser import Parser
 from components.parser import variables
+from components.codeGenerator import CodeGenerator
+from components.treeWalker import TreeWalker
+import sys
 
-
-fille = open("./tests/simpleSum.c")
-text = fille.read()
-lexer = Lexer()
-lexer.build()
-tokens = lexer.lexer.input(text)
-
-"""for token in lexer.lexer:
-    print (token)"""
-
-#input_str = [f"{tok[0]}:{tok[1]}" for tok in tokens]
-parser = Parser(lexer.tokens)
-test = parser.parse(text, lexer=lexer) 
-#test.print_tree()
-test.print_tree()
-
-print(variables)
-
-
-
-"""print(test.find_expression_with_binop(test))
-print(f'variables: {variables}')
-
-testeEXP = test.children[2].children[0].children[0].children[0].children[0]
-
-#print(testeEXP.find_expression_with_binop(testeEXP))
-
-nodeAtual = testeEXP.find_expression_with_binop(testeEXP)
-#print(nodeAtual.print_tree())
-lastNode = nodeAtual
-while nodeAtual:
-    nodeAtual = testeEXP.find_expression_with_binop(nodeAtual)
-    if(nodeAtual):
-        lastNode = nodeAtual
+LANGUAGE_EXTENCION = "c"
+def main(fille_name):
+    try:
+        #verifica de fille_name termina com .c
+        extencion = fille_name.split(".")[-1]
+        if(extencion != LANGUAGE_EXTENCION):
+            print("Fille extencion is not .c")
+            exit(1)
+        fille = open(f"./tests/{fille_name}")
+    except:
+        print("Fille not found")
+        exit(1)
         
-#print(lastNode.print_tree())
-test.resolve_binop_as_child(test)
-print"""
+    text = fille.read()
+    lexer = Lexer()
+    lexer.build()
+    lexer.lexer.input(text)
+
+    parser = Parser(lexer.tokens)
+    test = parser.parse(text, lexer=lexer) 
+
+    test.print_tree()
+
+    codeGenrator = CodeGenerator() 
+    walker = TreeWalker(codeGenrator,test)
+    walker.walk(test)
+    print(walker.codeGen.module)
+
+
+
+    
+
+if __name__ == "__main__":
+    fille_name = sys.argv[1]
+    main(fille_name)
